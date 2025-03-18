09f7e2f533fdb67cc46314653b410ad6c554e9ff
ZOE2K24 <jm.legaspi06@gmail.com>
2025-03-18 11:22:29 -0600

    Third Update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Import the express package
 const express = require('express');
 require('dotenv').config();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT; 
 
 // Initialize a new express instance
 const app = express();
@@ -11,7 +11,18 @@ app.get('/home', (req, res) => {
     res.send("Hello, World!");
 });
 
-// Start our server
+//route handler for a post request using path parameter
+app.post('/user/:id', (req, res) => {
+    const user_id = req.params.id;
+    res.send(`This is the user id: ${user_id}`); 
+});
+
+//route handler for a post request
+app.post('/response', (req, res)=>{
+    res.send('This is a post response in a RESTful style');
+});
+
+// Start our server, listening on port 3000
 app.listen(PORT, () => {
     console.log(`Connected on port ${PORT}`);
 });
